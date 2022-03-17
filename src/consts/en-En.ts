24cc514a125a34bd0a12d4ac5d94718b032c20bc
Amine <amine.ballalou@gmail.com>
2022-03-17 13:27:05 +0100

    [refactore] fix typo

--- src/consts/en-En.ts
+++ src/consts/en-En.ts
@@ -4,12 +4,12 @@ export default {
     durationInBed: {
       title: "How long did you stay in bed ?",
       placeholder: "Duration in bed",
-      error: "Duration in bed should be greater than Duration asleep",
+      error: "Duration in bed should be grater than Duration asleep",
     },
     durationAsleep: {
       title: "How long did you sleep ?",
       placeholder: "Duration asleep",
-      error: "Duration asleep should be greater than Duration in bed",
+      error: "Duration asleep should be grater than duration in bed",
     },
   },
   scoreScreen: {
